b4a87dcabd1120bd79c783bf3ae24dae09d2aa34
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2022-09-23 14:57:34 +0200

    config firebase

--- fonction.js
+++ fonction.js
@@ -1,1 +1,9 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-database.js';
+
+const firebaseConfig = {
+    databaseURL: "https://projetcrustace-default-rtdb.europe-west1.firebaseio.com/"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
