fb900146c1c126bbdb858915db61a4966c21e254
lolo298 <lorenzoaoulini@gmail.com>
2022-09-25 19:03:38 +0200

    infos crustaces

--- app.js
+++ app.js
@@ -7,7 +7,29 @@ window.onload = async () => {
   info.addEventListener("click", function () {
     let menu = document.getElementById("mainWrapper");
     menu.classList.add("info");
+    let menuContent = document.getElementById("info-content");
+    menuContent.classList.toggle("show");
   });
+
+  let data = await getAllData();
+  let rng = Math.floor(Math.random() * data.length);
+
+  let defaultData = data[rng];
+  let image = document.getElementById("crustace");
+  let name = document.getElementById("card").querySelector("h2");
+  if (rng == 7) {
+    name.style.transform = "translate(-70px, -100px)";
+  }
+  image.src = defaultData.image;
+  name.innerHTML = defaultData.name;
+  name = document.getElementById("name");
+  name.innerHTML = defaultData.name;
+  let taille = document.getElementById("capture");
+  let qte = document.getElementById("quantite");
+  let periode = document.getElementById("periode");
+  taille.innerHTML = defaultData.texts.taille;
+  qte.innerHTML = defaultData.texts.nombre;
+  periode.innerHTML = defaultData.texts.periode;
 };
 
 async function getAllData() {
