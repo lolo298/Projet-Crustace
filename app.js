c5841d25a4bf1f8d2c50141387284c75321be4bb
lolo298 <lorenzoaoulini@gmail.com>
2022-09-25 17:24:06 +0200

    Start of info page

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-window.onload = async function () {
-  let tmp = await getData();
+window.onload = async () => {
+  let divider = document.getElementById("divide");
+  let element = document.getElementById("card").querySelector("div");
+  divider.style.width = element.offsetWidth + 40 - 8 + "px";
+
+  let info = document.getElementById("info");
+  info.addEventListener("click", function () {
+    let menu = document.getElementById("mainWrapper");
+    menu.classList.add("info");
+  });
 };
+
+async function getAllData() {
+  let response = await getData();
+  let data = await response;
+  return data;
+}
